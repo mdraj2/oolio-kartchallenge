84516c4324fdbda383798a4deed9c562949eccdd
Malith Rajapakse <malithrajapakse@Maliths-MacBook-Pro-2.local>
2024-12-02 19:38:42 +1100

    enable cloudfront and remove fav icon

--- infra/main.tf
+++ infra/main.tf
@@ -59,7 +59,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     origin_access_control_id = aws_cloudfront_origin_access_control.default.id
     origin_id                = local.s3_origin_id
   }
-  enabled             = false
+  enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
   default_cache_behavior {
